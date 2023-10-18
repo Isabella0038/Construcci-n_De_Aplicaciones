d2aa4c30488c75619aee48314c37e17fd51275e0
Isabella0038 <142461650+Isabella0038@users.noreply.github.com>
2023-10-17 19:45:01 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace ARCHIVO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnAbrirPDF = new System.Windows.Forms.Button();
+            this.pdfViewer1 = new PdfiumViewer.PdfViewer();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAbrirPDF
+            // 
+            this.btnAbrirPDF.Location = new System.Drawing.Point(674, 583);
+            this.btnAbrirPDF.Name = "btnAbrirPDF";
+            this.btnAbrirPDF.Size = new System.Drawing.Size(134, 53);
+            this.btnAbrirPDF.TabIndex = 1;
+            this.btnAbrirPDF.Text = "CARGAR PDF";
+            this.btnAbrirPDF.UseVisualStyleBackColor = true;
+            this.btnAbrirPDF.Click += new System.EventHandler(this.btnAbrirPDF_Click);
+            // 
+            // pdfViewer1
+            // 
+            this.pdfViewer1.Location = new System.Drawing.Point(534, 82);
+            this.pdfViewer1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pdfViewer1.Name = "pdfViewer1";
+            this.pdfViewer1.Size = new System.Drawing.Size(441, 433);
+            this.pdfViewer1.TabIndex = 2;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(1018, 82);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(475, 475);
+            this.webBrowser1.TabIndex = 3;
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(40, 98);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(290, 310);
+            this.axWindowsMediaPlayer1.TabIndex = 4;
+            this.axWindowsMediaPlayer1.ClientSizeChanged += new System.EventHandler(this.Form1_Load);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "REPRODUCTOR DE MUSIC";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(683, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "LECTOR PDF";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1192, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(154, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "NAVEGADOR WEB";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1530, 844);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.pdfViewer1);
+            this.Controls.Add(this.btnAbrirPDF);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnAbrirPDF;
+        private PdfiumViewer.PdfViewer pdfViewer1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PdfiumViewer; // Importa la biblioteca PdfiumViewer para trabajar con archivos PDF.
+using WinRT.Controls; // Importa la biblioteca WinRT.Controls para utilizar controles WinRT.
+using AxWMPLib; // Importa la biblioteca AxWMPLib para trabajar con el reproductor de medios Windows Media Player.
+
+namespace ARCHIVO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load; // Suscribe el evento Load del formulario para realizar acciones cuando el formulario se carga.
+        }
+
+        private void btnAbrirPDF_Click(object sender, EventArgs e)
+        {
+            // Abre un cuadro de diálogo para seleccionar un archivo PDF.
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Archivos PDF (*.pdf)|*.pdf";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string pdfFilePath = openFileDialog.FileName;
+                // Llama a la función MostrarPDF para cargar y mostrar el archivo PDF seleccionado.
+                MostrarPDF(pdfFilePath); 
+            }
+        }
+
+        private void MostrarPDF(string filePath)
+        {
+            try
+            {
+                // Carga un archivo PDF desde la ubicación especificada.
+                PdfDocument pdfDocument = PdfDocument.Load(filePath);
+                // Asigna el documento PDF al control PdfViewer para su visualización.
+                pdfViewer1.Document = pdfDocument; 
+            }
+            catch (Exception ex)
+            {
+                // En caso de error al abrir el archivo PDF, muestra un mensaje de error.
+                MessageBox.Show("Error al abrir el archivo PDF: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Cargar y reproducir un archivo multimedia
+            string rutaDelArchivoMultimedia = @"C:\Users\Asus\Downloads\musica\Bob Marley - Is This Love (320 kbps).mp3";
+            axWindowsMediaPlayer1.URL = rutaDelArchivoMultimedia;
+
+
+            // Cargar una página web en el WebBrowser
+            webBrowser1.Navigate("https://www.google.com");
+
+
+        }
+    }
+}
